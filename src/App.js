a822b43c200e31ec05f4644d6f9b34c883e075d3
adman94 <adman00794@gmail.com>
2020-08-16 19:39:33 -0700

    basic blog post component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import './App.css';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
+
 
 function App() {
-
-  const buttonText = { text: 'Click me' }
-  const style = { backgroundColor: '#32A9B3', color: 'white' }
-  const labelText = 'Enter name: '
-
   return (
-    <div className="App">
-      <div>
-        <label className="label" htmlFor="name">
-          {labelText}
-        </label>
-        <input id="name" type="text" />
-        <button style={style}>{buttonText.text}</button>
-      </div>
+    <div className="ui container comments">
+      <CommentDetail 
+        name="Alex" 
+        timeAgo="Today at 3:45PM"
+        content="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        name="Mike" 
+        timeAgo="Today at 5:00PM "
+        content="I like reading..."
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        name="George" 
+        timeAgo="Today at 2:00AM"
+        content="The weather is really hot!!"
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        name="Lucas" 
+        timeAgo="Today at 11:35AM"
+        content="Your coffee is getting cold"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        name="Timothy" 
+        timeAgo="Today at 8:00PM"
+        content="What's wrong with you?"
+        avatar={faker.image.avatar()} 
+      />
     </div>
   );
 }
