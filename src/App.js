8e1811b0ddd3477fbe04178dbf6e96f16ab69ec3
Adman94 <adman00794@gmail.com>
2021-05-31 07:07:20 -0700

    latest commit 05/31

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import unsplash from './api/unsplash';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 class App extends Component {
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('https://api.unsplash.com/search/photos', {
+      params: { query: term }
+    })
+    this.setState({ images: response.data.results });
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
